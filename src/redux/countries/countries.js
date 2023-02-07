7ed00b9a4556e72bc7276e9d047e057eb0c58e0c
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-07 20:37:26 +0600

    Add Api links and also add dummy data in countries reducer

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,7 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { mapBaseUrl } from '../../api/api';
+import AsiaMap from '../../assets/images/asia.png';
+
 const initialState = {
-  countries: [],
+  region: {
+    name: 'Asia',
+    regionMap: AsiaMap,
+    totalCountries: 52,
+    totalPopulation: 5555555555,
+  },
+  countries: [
+    {
+      id: '112',
+      name: 'Banglasesh',
+      capital: 'Dhaka',
+      population: 164689383,
+      cc: 'BD',
+      flag: 'https://flagcdn.com/bd.svg',
+      map: `${mapBaseUrl}bd/vector.svg`,
+      lat: 24,
+      lon: 90,
+    },
+    {
+      id: '113',
+      name: 'Banglasesh',
+      capital: 'Dhaka',
+      population: 164689383,
+      cc: 'BD',
+      flag: 'https://flagcdn.com/bd.svg',
+      map: `${mapBaseUrl}bd/vector.svg`,
+      lat: 24,
+      lon: 90,
+    },
+    {
+      id: '114',
+      name: 'Banglasesh',
+      capital: 'Dhaka',
+      population: 164689383,
+      cc: 'BD',
+      flag: 'https://flagcdn.com/bd.svg',
+      map: `${mapBaseUrl}bd/vector.svg`,
+      lat: 24,
+      lon: 90,
+    },
+  ],
 };
 
 const countriesSlice = createSlice({
